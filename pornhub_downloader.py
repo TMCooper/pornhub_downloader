e4d70332de8146c0c09ed36af9df454e7074aabb
TMCooper# <pokemon160377@gmail.com>
2024-08-18 04:56:25 +0200

    Code Update

--- pornhub_downloader.py
+++ pornhub_downloader.py
@@ -1,12 +1,15 @@
-from pornhub_api import PornhubApi
+import phub
+from phub import *
+import os
 
-api = PornhubApi()
 
-videos = api.search_videos.search_videos(
-    "chechick",
-    ordering="mostviewed",
-    period="weekly",
-    tags=["black"],
-)
-for vid in videos:
-    print(vid.title, vid.video_id)
+PATH = os.path.dirname(os.path.abspath(__file__))
+PATH_VID = os.path.join(PATH, "Vidéo")
+
+Link_porn = input("Quelle est le lien que vous souhaité télécharger ? : ")
+
+client = phub.Client()
+
+video = client.get('https://fr.pornhub.com/view_video.php?viewkey=669e868825c52')
+video.download('my-video.mp4', Quality.BEST)
+
