beb91ba9e9136e01ca090d9526ebcd822347055d
TMCooper# <pokemon160377@gmail.com>
2024-08-18 06:31:42 +0200

    Code Update

--- pornhub_downloader.py
+++ pornhub_downloader.py
@@ -5,11 +5,28 @@ import os
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 PATH_VID = os.path.join(PATH, "Vidéo")
+EXTENTION = ".mp4"
+QUALITE = Quality.BEST
 
-Link_porn = input("Quelle est le lien que vous souhaité télécharger ? : ")
+def main ():
+    client = phub.Client()
+    
+    choix = input("Voulez vous télécharger une viéo ou une playlist ? : ")
 
-client = phub.Client()
+    while choix not in ["V", "v", "P", "p"]:
+        choix = input("Veulliez saisir V ou P : ")
 
-video = client.get('https://fr.pornhub.com/view_video.php?viewkey=669e868825c52')
-video.download('my-video.mp4', Quality.BEST)
+    if choix in ["V", "v"]:
 
+        Link_porn = input("Quelle est le lien que vous souhaité télécharger ? : ")
+
+        if not os.path.exists(PATH_VID):
+            os.mkdir(PATH_VID)
+    
+        video = client.get(Link_porn)
+        titre = PATH_VID+"\\"+video.title+EXTENTION
+        video.download(titre, QUALITE)
+    
+
+if __name__ == "__main__":
+    main()
