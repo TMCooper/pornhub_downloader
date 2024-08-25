c84d28139079043f3220af0cdf5c8cc319d565ec
TMCooper# <pokemon160377@gmail.com>
2024-08-26 01:03:06 +0200

    Update Code
    Update for english people

--- pornhub_downloader.py
+++ pornhub_downloader.py
@@ -15,16 +15,33 @@ QUALITE = Quality.BEST
 def main ():
     try:
         client = phub.Client()
+        
+        lang = input("What is your language ? (FR/EN) : ").lower()
 
-        print("Appuier sur Q si vous voulez quitté le programme")    
-        choix = input("Voulez vous télécharger une viéo ou une playlist ? V/P : ").lower()
+        while lang not in ["fr", "en"]:
+            lang = input("What is your language ? FR or EN").lower()
+        
+        if lang in["en"]:
+            print("Press Q if you want to exit the program")
+            choix = input("Do you want to download a video or a playlist? V/P: ").lower()
+
+        else:
+            print("Appuier sur Q si vous voulez quitté le programme")
+            choix = input("Voulez vous télécharger une viéo ou une playlist ? V/P : ").lower()
 
         while choix not in ["v", "p", "q"]:
-            choix = input("Veulliez saisir V ou P : ").lower()
+            if lang in["en"]:
+                choix = input("Please enter V or P: ").lower()
+
+            else:
+                choix = input("Veulliez saisir V ou P : ").lower()
 
         if choix in ["v"]:
 
-            Link_porn = input("Quelle est le lien que vous souhaité télécharger ? : ")
+            if lang in["en"]:
+                Link_porn = input("What is the link you want to download ? : ")
+            else:
+                Link_porn = input("Quelle est le lien que vous souhaité télécharger ? : ")
 
             if not os.path.exists(PATH_VID):
                 os.mkdir(PATH_VID)
@@ -35,7 +52,11 @@ def main ():
         
         if choix in ["p"]:
 
-            Link_porn = input("Quel est le lien que vous souhaité télécharger ? : ")
+            if lang in ["en"]:
+                Link_porn = input("What is the link you want to download? : ")
+
+            else:
+                Link_porn = input("Quel est le lien que vous souhaité télécharger ? : ")
 
             if not os.path.exists(PATH_PLAY):
                     os.mkdir(PATH_PLAY)
@@ -78,13 +99,19 @@ def main ():
                         titre = PATH_PLAY+"\\"+video.title+EXTENTION
                         video.download(titre, QUALITE)
                 except IndexError:
-                    print("Erreur le lien que vous avez fournit n'est pas une playlist veulliez saisir la bonne option a l'avenir..")
+                    if lang in ["en"]:
+                        print("\nError the link you provided is not a playlist please enter the correct option in the future...")
+                    else:
+                        print("\nErreur le lien que vous avez fournit n'est pas une playlist veulliez saisir la bonne option a l'avenir..")
                     return
 
         if choix in ["q"]:
-            exit      
+            exit()
     except KeyboardInterrupt:
-        print("\nCTRL + C saisit par l'utilisateur arret du programme...")
+        if lang in ["en"]:
+            print("\nCTRL + C entered by the user to stop the program...")
+        else:
+            print("\nCTRL + C saisit par l'utilisateur arret du programme...")
 
 if __name__ == "__main__":
     main()
